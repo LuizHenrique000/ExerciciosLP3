bbbeb27cd1fd1adc94c59f550b5a55bafde5ed83
LuizHenrique000 <luizlhra24@gmail.com>
2022-10-01 12:41:52 -0300

    Implementa metodos restantes do API Streams

--- ExercicioAvaliativo1/src/exercicioAvaliativo/java8/TopicoJava8.java
+++ ExercicioAvaliativo1/src/exercicioAvaliativo/java8/TopicoJava8.java
@@ -12,21 +12,43 @@ public class TopicoJava8 {
 		String pokemon4 = "Psyduck";
 
 		ArrayList<String> pokemons = new ArrayList<>();
-		
+
 		pokemons.add(pokemon1);
 		pokemons.add(pokemon2);
 		pokemons.add(pokemon3);
 		pokemons.add(pokemon4);
 		System.out.println(pokemons);
-		
-		//Count
-		
-		long totalPokemons = pokemons.stream().count();
+
+		// Count
+		long totalPokemons = 
+				pokemons
+				.stream()
+				.count();
 		System.out.println("Numero de pokemons: " + totalPokemons);
-		
-		//Limit
-		
-		Stream<String> novosPokemons = pokemons.stream().limit(2);
+
+		// Limit
+		Stream<String> novosPokemons = 
+				pokemons
+				.stream()
+				.limit(2);
 		System.out.println(novosPokemons.toString());
+
+		// ForEach
+		pokemons
+		.stream()
+		.forEach(e -> System.out.print(e + " "));
+
+		// Filter
+		pokemons.
+		stream()
+		.filter(e -> e == "Pikachu")
+		.forEach(e -> System.out.print(e + " "));
+		
+		// Map
+		pokemons
+		.stream()
+		.map(e -> e + "Pokemon: " + " ")
+		.forEach(e -> System.out.print(e + " "));
+		
 	}
 }
