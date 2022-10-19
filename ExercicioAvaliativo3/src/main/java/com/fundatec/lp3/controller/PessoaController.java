e8e521c62b932cbb10b86a153811a2642c650022
LuizHenrique000 <luizlhra24@gmail.com>
2022-10-18 22:05:46 -0300

    Implementa controller

--- ExercicioAvaliativo3/src/main/java/com/fundatec/lp3/controller/PessoaController.java
+++ ExercicioAvaliativo3/src/main/java/com/fundatec/lp3/controller/PessoaController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.fundatec.lp3.models.Pessoa;
 import com.fundatec.lp3.repository.PessoaRepository;
@@ -22,7 +21,7 @@ public class PessoaController {
 	@Autowired
 	PessoaService pessoaService;
 	
-	@PostMapping(value = "/pessoa", method =  RequestMethod.POST)
+	@PostMapping(value = "/pessoa")
 	public ResponseEntity<Pessoa> adicionarPessoa(@RequestBody Pessoa pessoa) {
 		Pessoa pessoaRegistrada = pessoaService.salvar(pessoa);
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaRegistrada);
